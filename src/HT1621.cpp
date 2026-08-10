46f000d3d119e4dfb521f3f09c2d18dfde3c5fb6
Jarx12 <juanruiz09@protonmail.com>
2026-08-10 14:01:17 -0400

    first commit

---
+++ src/HT1621.cpp
@@ -0,0 +1,486 @@
+/*******************************************************************************
+Copyright 2016-2018 anxzhu (github.com/anxzhu)
+Copyright 2018-2020 Valerio Nappi (github.com/5N44P) (changes)
+Based on segment-lcd-with-ht1621 from anxzhu (2016-2018)
+(https://github.com/anxzhu/segment-lcd-with-ht1621)
+
+Partially rewritten and extended by Valerio Nappi (github.com/5N44P) in 2018
+
+This file is part of the HT1621 arduino library, and thus under the MIT license.
+More info on the project and the license conditions on :
+https://github.com/5N44P/ht1621-7-seg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*******************************************************************************/
+
+#include <Arduino.h>
+#include "HT1621.h"
+
+HT1621::HT1621() {
+    memset(_buffer, 0x00, sizeof(_buffer));
+}
+
+
+void HT1621::begin(int cs_p, int wr_p, int data_p, int backlight_p)
+{
+	pinMode(cs_p, OUTPUT);
+	pinMode(wr_p, OUTPUT);
+	pinMode(data_p, OUTPUT);
+	pinMode(backlight_p, OUTPUT);
+	_cs_p=cs_p;
+	_wr_p=wr_p;
+	_data_p=data_p;
+	_backlight_p=backlight_p;
+	_backlight_en=true;
+	config();
+}
+
+void HT1621::begin(int cs_p, int wr_p, int data_p)
+{
+	pinMode(cs_p, OUTPUT);
+	pinMode(wr_p, OUTPUT);
+	pinMode(data_p, OUTPUT);
+	_cs_p=cs_p;
+	_wr_p=wr_p;
+	_data_p=data_p;
+	_backlight_en = false;
+	config();
+}
+
+void HT1621::wrDATA(unsigned char data, unsigned char cnt) {
+	unsigned char i;
+	for (i = 0; i < cnt; i++) {
+		digitalWrite(_wr_p, LOW);
+		delayMicroseconds(4);
+		if (data & 0x80) {
+			digitalWrite(_data_p, HIGH);
+		}
+		else {
+			digitalWrite(_data_p, LOW);
+		}
+		digitalWrite(_wr_p, HIGH);
+		delayMicroseconds(4);
+		data <<= 1;
+	}
+}
+void HT1621::wrclrdata(unsigned char addr, unsigned char sdata)
+{
+	addr <<= 2;
+	digitalWrite(_cs_p, LOW);
+	wrDATA(0xa0, 3);
+	wrDATA(addr, 6);
+	wrDATA(sdata, 8);
+	digitalWrite(_cs_p, HIGH);
+}
+
+void HT1621::display()
+{
+	wrCMD(LCDON);
+}
+
+void HT1621::noDisplay()
+{
+	wrCMD(LCDOFF);
+}
+
+void HT1621::wrone(unsigned char addr, unsigned char sdata)
+{
+	addr <<= 2;
+	digitalWrite(_cs_p, LOW);
+	wrDATA(0xa0, 3);
+	wrDATA(addr, 6);
+	wrDATA(sdata, 8);
+	digitalWrite(_cs_p, HIGH);
+}
+
+void HT1621::backlight()
+{
+	if (_backlight_en)
+		digitalWrite(_backlight_p, HIGH);
+	delay(1);
+}
+
+void HT1621::noBacklight()
+{
+	if(_backlight_en)
+		digitalWrite(_backlight_p, LOW);
+	delay(1);
+}
+
+void HT1621::wrCMD(unsigned char CMD) {  //100
+	digitalWrite(_cs_p, LOW);
+	wrDATA(0x80, 4);
+	wrDATA(CMD, 8);
+	digitalWrite(_cs_p, HIGH);
+}
+
+void HT1621::config()
+{
+	wrCMD(BIAS);
+	wrCMD(RC256);
+	wrCMD(SYSDIS);
+	wrCMD(WDTDIS1);
+	wrCMD(SYSEN);
+	wrCMD(LCDON);
+}
+
+void HT1621::wrCLR(unsigned char len) {
+	unsigned char addr = 0;
+	unsigned char i;
+	for (i = 0; i < len; i++) {
+		wrclrdata(addr, 0x00);
+		addr = addr + 2;
+	}
+}
+
+void HT1621::setBatteryLevel(int level) {
+	// zero out the previous (otherwise the or couldn't be possible)
+	_buffer[0] &= 0x7F;
+	_buffer[1] &= 0x7F;
+	_buffer[2] &= 0x7F;
+
+	switch(level){
+		case 3: // battery on and all 3 segments
+			_buffer[0] |= 0x80;
+		case 2: // battery on and 2 segments
+			_buffer[1] |= 0x80;
+		case 1: // battery on and 1 segment
+			_buffer[2] |= 0x80;
+		case 0: // battery indication off
+		default:
+			break;
+	}
+
+	update();
+}
+
+void HT1621::clear() {
+    // 1. Clear the hardware display memory (16 addresses: 0x00 through 0x1E)
+    wrCLR(16);
+
+    // 2. Clear the local display buffer
+    for (uint8_t i = 0; i < BUFFERSIZE; i++) {
+        _buffer[i] = 0x00;
+    }
+}
+
+// takes the buffer and puts it straight into the driver
+void HT1621::update(){
+	// the buffer is backwards with respect to the lcd. could be improved
+	wrone(0, _buffer[5]);
+	wrone(2, _buffer[4]);
+	wrone(4, _buffer[3]);
+	wrone(6, _buffer[2]);
+	wrone(8, _buffer[1]);
+	wrone(10,_buffer[0]);
+}
+
+void HT1621::print(long num, const char* flags, int precision){
+	if(num > 999999) // basic checks
+		num = 999999; // clip into 999999
+	if(num < -99999) // basic checks
+		num = -99999; // clip into -99999
+
+	char localbuffer[7]; //buffer to work within the function
+	snprintf(localbuffer, 7, flags, num); // convert the decimal into string
+	#ifdef _HTDEBUG
+		Serial.begin(9600);
+		Serial.print(localbuffer);
+		Serial.print("\t");
+	#endif
+
+	// horrible handling but should get us working. needs refactor in next major
+	if (precision > 0 && (num) < pow(10, precision)) {
+		// we remove extra leading zeros
+		for (int i = 0; i < (5 - precision); i++) {
+			#ifdef _HTDEBUG
+				Serial.print(localbuffer[1]);
+			#endif // _HTDEBUG
+			if(localbuffer[i+1] == '0' && localbuffer[i] != '-'){ // we remove only if there is another zero ahead AND if it's not a minus sign
+				localbuffer[i] = ' ';
+			}
+			else{
+				break;
+			} 
+			#ifdef _HTDEBUG
+				Serial.println();buffer[1]);
+			#endif // _HTDEBUG
+	}
+	}
+
+
+	for(int i=0; i<6; i++){
+		_buffer[i] &= 0x80; // mask the first bit, used by batter and decimal point
+		_buffer[i] |= charToSegBits(localbuffer[i]);
+	}
+	update();
+}
+
+void HT1621::print(double num, int precision){
+	if(num > 999999) // basic checks
+		num = 999999; // clip into 999999
+	if(num < -99999) // basic checks
+		num = -99999; // clip into -99999
+
+	if(precision > 3 && num > 0)
+		precision = 3; // if positive max precision allowed = 3
+	else if(precision > 2 && num < 0)
+		precision = 2;// if negative max precision allowed = 2
+	if(precision < 0)
+		precision = 0; // negative precision?!
+
+	const char* flags = (precision > 0 && abs(num) < 1) ? "%06li" : "%6li";
+
+	long integerpart;
+	integerpart = ((long)(num*pow(10,precision)));
+
+	print(integerpart, flags, precision); // draw the integerized number
+	setdecimalseparator(precision); // draw the decimal point
+
+	update();
+}
+
+
+void HT1621::printCelsius(double num){
+	if(num > 9999) // basic checks
+		num = 9999; // clip into 999999
+	if(num < -999) // basic checks
+		num = -999; // clip into -99999
+
+	int precision;
+	
+	if(num <= -100 || num >= 999)
+		precision = 0;	// if negative max precision allowed = 0
+	else 
+		precision = 1;	// if positive max precision allowed = 1
+
+	const char* flags = (precision > 0 && abs(num) < 1) ? "%04li*C" : "%4li*C";
+
+	long integerpart;
+	integerpart = ((long)(num*pow(10,precision)));
+
+
+	print(integerpart, flags, precision); // draw the integerized number
+	if(precision > 0)
+		setdecimalseparator(precision+2); // draw the decimal point shifted by 2
+	else 	
+		setdecimalseparator(0); // or clear the decimal separator
+
+	update();
+}
+
+void HT1621::print(const char* str, bool leftPadded){
+	int chars = strlen(str);
+	int padding = 6 - chars;
+
+	for(int i = 0; i < 6; i++){
+		_buffer[i] &= 0x80; // mask the first bit, used by batter and decimal point
+		char character = leftPadded
+				 		 ? i < padding ? ' ' : str[i - padding]
+				 		 : i >= chars ? ' ' : str[i];
+		_buffer[i] |= charToSegBits(character);
+	}
+
+	setdecimalseparator(0); // Hide decimal point
+	update();
+}
+
+// ==========================================
+// LEFT DISPLAY (5 Digits) (Addr 30 to 22)
+// ==========================================
+void HT1621::printLeft(long num) {
+    char buf[6];
+    snprintf(buf, sizeof(buf), "%5ld", num); // Right-aligned 5 digits
+    printLeft(buf);
+}
+
+void HT1621::printLeft(const char* str) {
+    static const uint8_t left_addrs[5] = { 30, 28, 26, 24, 22 };
+    int len = strlen(str);
+    for (int i = 0; i < 5; i++) {
+        char c = (i < len) ? str[i] : ' ';
+        uint8_t segs = charToSegBits(c);
+        wrone(left_addrs[i], segs);
+    }
+}
+
+// ==========================================
+// MIDDLE DISPLAY (5 Digits) (Addr 20 to 12)
+// ==========================================
+void HT1621::printMid(long num) {
+    char buf[6];
+    snprintf(buf, sizeof(buf), "%5ld", num); // Right-aligned 5 digits
+    printMid(buf);
+}
+
+void HT1621::printMid(const char* str) {
+    static const uint8_t mid_addrs[5] = { 20, 18, 16, 14, 12 };
+    int len = strlen(str);
+    for (int i = 0; i < 5; i++) {
+        char c = (i < len) ? str[i] : ' ';
+        uint8_t segs = charToSegBits(c);
+        wrone(mid_addrs[i], segs);
+    }
+}
+
+// ==========================================
+// RIGHT DISPLAY (6 Digits) (Addr 10 to 0)
+// ==========================================
+void HT1621::printRight(long num) {
+    char buf[7];
+    snprintf(buf, sizeof(buf), "%6ld", num); // Right-aligned 6 digits
+    printRight(buf);
+}
+
+void HT1621::printRight(const char* str) {
+    static const uint8_t right_addrs[6] = { 10, 8, 6, 4, 2, 0 };
+    int len = strlen(str);
+    for (int i = 0; i < 6; i++) {
+        char c = (i < len) ? str[i] : ' ';
+        uint8_t segs = charToSegBits(c);
+        wrone(right_addrs[i], segs);
+    }
+}
+
+
+void HT1621::setdecimalseparator(int decimaldigits) {
+	// zero out the eight bit
+	_buffer[3] &= 0x7F;
+	_buffer[4] &= 0x7F;
+	_buffer[5] &= 0x7F;
+
+	if( decimaldigits <= 0 || decimaldigits > 3){
+		return;
+	}
+
+	// 3 is the digit offset
+	// the first three eights bits in the buffer are for the battery signs
+	// the last three are for the decimal point
+	_buffer[6-decimaldigits] |= 0x80;
+}
+
+char HT1621::charToSegBits(char character) {
+	switch (character) {
+	case '*': // For degree for now
+		return 0b0110011;
+	case '|':
+		return 0b0000101;
+	case '-':
+		return 0b0000010;
+	case '_':
+		return 0b0001000;
+	case '0':
+		return 0b1111101;
+	case '1':
+		return 0b1100000;
+	case '2':
+		return 0b111110;
+	case '3':
+		return 0b1111010;
+	case '4':
+		return 0b1100011;
+	case '5':
+		return 0b1011011;
+	case '6':
+		return 0b1011111;
+	case '7':
+		return 0b1110000;
+	case '8':
+		return 0b1111111;
+	case '9':
+		return 0b1111011;
+	case 'A':
+	case 'a':
+		return 0b1110111;
+	case 'b':
+	case 'B':
+		return 0b1001111;
+	case 'c':
+	//	return 0b0001110;
+	case 'C':
+		return 0b0011101;
+	case 'd':
+	case 'D':
+		return 0b1101110;
+	case 'e':
+	//	return 0b0001110;
+	case 'E':
+		return 0b0011111;
+	case 'f':
+	//	return 0b0000111;
+	case 'F':
+		return 0b0010111;
+	case 'G':
+	case 'g':
+		return 0b1011101;
+	case 'h':
+	//	return 0b1000111;
+	case 'H':
+		return 0b1100111;
+	case 'i':
+	//	return 0b1000000;
+	case 'I':
+		return 0b1100000;
+	case 'J':
+	case 'j':
+		return 0b1101000;
+	case 'l':
+	//	return 0b1100000;
+	case 'L':
+		return 0b0001101;
+	case 'm':
+	case 'M':
+		return 0b1010100;
+	case 'n':
+	case 'N':
+		return 0b1000110;
+	case 'O': // we can keep this for zero
+	//	return 0b1111101;
+	case 'o':
+		return 0b1001110;
+	case 'P':
+	case 'p':
+		return 0b0110111;
+	case 'q':
+	case 'Q':
+		return 0b1110011;
+	case 'r':
+	case 'R':
+		return 0b0000110;
+	case 'S':
+	case 's':
+		return 0b1011011;
+	case 't':
+	case 'T':
+		return 0b0001111;
+	case 'u':
+	//	return 0b1001100;
+	case 'U':
+		return 0b1101101;
+	case 'Y':
+	case 'y':
+		return 0b1101011;
+	case 'z':
+	case 'Z':
+		return 0b0111110;
+	case ' ':
+	default:
+		return 0b0000000;
+	}
+}
---
+++ src/main.cpp
@@ -0,0 +1,145 @@
+#include <Arduino.h>
+#include "HT1621.h"
+
+HT1621 lcd;
+
+
+const uint8_t CS_PIN   = 7;
+const uint8_t WR_PIN   = 5;
+const uint8_t DATA_PIN = 4;
+const uint8_t BLK_PIN  = 8; 
+//const uint8_t TOUCHIO_PIN  = 8; 
+const uint8_t BACKLIGHT_PIN = 8;
+/*const int offsetUTC = -4;
+bool lastState = HIGH;
+uint8_t flag=0;
+unsigned long lastmilis = 0;
+unsigned long lastmilislcd = 0;
+unsigned long lastmilisclock = 0;
+
+
+
+
+void setBLK(int level)
+{
+  const int brightnessTable[] = {0, 50, 200, 500, 800, 1500, 3000, 4095};
+  if (level >= 0 && level <= 7)
+  {
+    ;  
+  } 
+  
+  //ledcWrite(BLK_PIN, brightnessTable[level]);
+}
+
+
+
+void apagarsimbolitosmidright() //Apagamos los simbolos de los segmentos del medio y de la derecha del lcd
+{
+    //lcd.SetmidTriangle(1,0); //Apagar el triangulo TOP
+    //lcd.SetmidTriangle(0,0); //Apagar el triangulo BOTTOM
+    for (int j=1;j<3;j++)  //Apagamos los puntos decimales
+      {
+        for (int i=1; i<4;i++)
+        {
+          //lcd.setDecimal(j,i,0);
+        }
+      }
+    lcd.setBatteryLevel(0); //Apagamos la barra de bateria
+}
+
+void utc_to_local_time(uint8_t &hora, int offset)
+{
+  bool sameday;
+  if (offset == 0)
+    ;
+  else if (offset < 0) //Retroceder
+  {
+    sameday = (hora >= abs(offset));
+    hora = sameday ? (hora - abs(offset)) : (hora + 24 - abs(offset));
+  }
+  else //Avanzar
+  {
+    sameday = (hora + abs(offset) < 24);
+    hora = sameday ? (hora + abs(offset)) : (hora + abs(offset) - 24);
+  }
+}
+
+
+void IOCambiarDatos()
+{
+  int touch = digitalRead(TOUCHIO_PIN);
+  if (touch == HIGH && lastState == LOW) //Si se toca el pin y no se ha dejado pegado en HIGH
+  {
+    flag++; //Increase Flag
+    if (flag > 2)
+      flag = 0;
+    delay(20); //Debounce
+  }
+  lastState = touch; //Almacenar el estado para comparar el siguiente ciclo
+}
+
+
+void lcd_showtime()
+{
+  unsigned long currentTime = millis();
+  if (currentTime - lastmilislcd >= 100) //Si han pasado mas de 100 ms
+      {
+        lastmilislcd = currentTime;
+        //;  
+        if (flag==0)
+        {
+          apagarsimbolitosmidright(); //Apagar los simbolos usados por el print_lcd_latlong() 
+          //print_lcd_time();
+          //print_lcd_date();
+        }
+        else if (flag==1)
+          ;  
+          //print_lcd_latlong();
+        else
+        {
+          apagarsimbolitosmidright(); //Apagar los simbolos usados por el print_lcd_latlong() 
+          //print_lcd_time();
+          //lcd.printRight("RPM 12"); //Presentar en pantalla con 3 decimales
+        }          
+      }
+}*/
+
+
+void testPanelCounters() {
+    static long leftCounter  = 0;
+    static long midCounter   = 1000;
+    static long rightCounter = 50000;
+
+    // Print counter values to each LCD panel
+    lcd.printLeft(leftCounter);
+    lcd.printMid(midCounter);
+    lcd.printRight(rightCounter);
+
+    // Increment values
+    leftCounter++;
+    midCounter += 2;
+    rightCounter += 5;
+
+    // Reset counters if they exceed panel display limits
+    if (leftCounter > 99999)    leftCounter = 0;   // 5 digits max
+    if (midCounter > 99999)     midCounter = 0;    // 5 digits max
+    if (rightCounter > 999999)  rightCounter = 0;  // 6 digits max
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  Serial.println(F("===================================="));
+  Serial.println(F(" HT1621 Multi-Display Section Test  "));
+  Serial.println(F("===================================="));
+
+  // Initialize HT1621 driver
+  lcd.begin(CS_PIN, WR_PIN, DATA_PIN);
+  lcd.clear();
+  delay(1000);
+}
+
+void loop() {
+  testPanelCounters();
+  delay (150);
+}
